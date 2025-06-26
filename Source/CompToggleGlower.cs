a4924c52fd1c8c45b241767110f5bc0cffe623e8
Harry Gillanders <contact@harrygillanders.com>
2025-06-27 00:29:38 +0100

    Add support for disabling the glow of a fridge.

--- Source/CompToggleGlower.cs
+++ Source/CompToggleGlower.cs
@@ -10,15 +10,39 @@ using Verse;
 namespace RimFridge
 {
 	[StaticConstructorOnStartup]
-	static class IconUtil
+	static internal class FridgeLighting
 	{
-		public static Texture2D Normal;
-		public static Texture2D Dark;
-
-		static IconUtil ()
+		internal enum LightType : byte
 		{
-			Normal = ContentFinder<Texture2D>.Get("UI/Icons/normal", true);
-			Dark = ContentFinder<Texture2D>.Get("UI/Icons/dark", true);
+			Normal = 0,
+			Dark = 1,
+			Off = 2,
+			Last = Off
+		}
+
+		internal static Texture2D[] iconsByLightType;
+		internal static ColorInt[] coloursByLightType;
+
+		static FridgeLighting ()
+		{
+			iconsByLightType = new Texture2D[3];
+			iconsByLightType[(uint) LightType.Normal] = ContentFinder<Texture2D>.Get("UI/Icons/normal", true);
+			iconsByLightType[(uint) LightType.Dark] = ContentFinder<Texture2D>.Get("UI/Icons/dark", true);
+			iconsByLightType[(uint) LightType.Off] = ContentFinder<Texture2D>.Get("UI/Icons/off", true);
+
+			coloursByLightType = new ColorInt[3];
+			coloursByLightType[(uint) LightType.Normal] = new ColorInt(89, 188, 255, 0);
+			coloursByLightType[(uint) LightType.Dark] = new ColorInt(78, 226, 229, 0);
+			coloursByLightType[(uint) LightType.Off] = new ColorInt(0, 0, 0, 0);
+		}
+
+		internal static LightType CycleLightType (LightType type)
+		{
+			uint nextType = (uint) type + 1;
+			nextType += ((nextType == (uint) LightType.Dark) & !ModsConfig.IdeologyActive) ? (uint) 1 : (uint) 0;
+			nextType = nextType <= (uint) LightType.Last ? nextType : (uint) LightType.Normal;
+
+			return (LightType) nextType;
 		}
 	}
 
@@ -32,58 +56,91 @@ namespace RimFridge
 
 	sealed class CompToggleGlower : CompGlower
 	{
-		bool isDarklight = false;
+		FridgeLighting.LightType lightType;
 
 		public override IEnumerable<Gizmo> CompGetGizmosExtra ()
 		{
 			foreach (var g in base.CompGetGizmosExtra())
 				yield return g;
 
-			if (ModsConfig.IdeologyActive)
+			Texture2D icon = FridgeLighting.iconsByLightType[(uint) lightType];
+
+			yield return new Command_Action
 			{
-				Texture2D icon;
-
-				if (isDarklight)
-					icon = IconUtil.Dark;
-				else
-					icon = IconUtil.Normal;
-
-				yield return new Command_Action
+				action = delegate
 				{
-					action = delegate
-					{
-						Verse.Sound.SoundStarter.PlayOneShotOnCamera(RimWorld.SoundDefOf.Tick_High);
-						isDarklight = !isDarklight;
-						SetLightColor();
-						base.parent.Map.glowGrid.DeRegisterGlower(this);
-						base.parent.Map.glowGrid.RegisterGlower(this);
-					},
-					defaultLabel = "RimFridge.ToggleGlowColor".Translate(),
-					defaultDesc = "RimFridge.ToggleGlowColorDesc".Translate(),
-					icon = icon
-				};
+					Verse.Sound.SoundStarter.PlayOneShotOnCamera(RimWorld.SoundDefOf.Tick_High);
+					this.CycleLightType();
+				},
+				defaultLabel = "RimFridge.ToggleGlowColor".Translate(),
+				defaultDesc = "RimFridge.ToggleGlowColorDesc".Translate(),
+				icon = icon
+			};
+		}
+
+		protected override bool ShouldBeLitNow
+		{
+			get
+			{
+				if (this.lightType == FridgeLighting.LightType.Off)
+				{
+					return false;
+				}
+
+				return base.ShouldBeLitNow;
 			}
 		}
 
 		public override void PostSpawnSetup (bool respawningAfterLoad)
 		{
 			base.PostSpawnSetup(respawningAfterLoad);
-			SetLightColor();
-
+			this.SetLightTypeTo(this.lightType);
 		}
 
-		private void SetLightColor ()
+		private void CycleLightType ()
 		{
-			if (isDarklight)
-				base.Props.glowColor = new ColorInt(78, 226, 229, 0);
-			else
-				base.Props.glowColor = new ColorInt(89, 188, 255, 0); // new ColorInt(252, 187, 113, 0);
+			this.SetLightTypeTo(FridgeLighting.CycleLightType(this.lightType));
+		}
+
+		private void SetLightTypeTo (FridgeLighting.LightType type)
+		{
+			this.lightType = type;
+			base.Props.glowColor = FridgeLighting.coloursByLightType[(uint) type];
+			base.parent.Map.glowGrid.DeRegisterGlower(this);
+			base.parent.Map.glowGrid.RegisterGlower(this);
 		}
 
 		public override void PostExposeData ()
 		{
 			base.PostExposeData();
-			Scribe_Values.Look(ref isDarklight, "isDarklight");
+
+			if (Scribe.mode == LoadSaveMode.Saving)
+			{
+				Scribe_Values.Look(ref this.lightType, "lightType", FridgeLighting.LightType.Normal, true);
+			}
+			else if (Scribe.mode == LoadSaveMode.LoadingVars)
+			{
+				/* Before version 2.0.0 of RimFridge, there were only two light-types,
+				   and the active type was persisted via the `isDarklight` boolean,
+				   hence we use a light-type of 255 to migrate old glowers. */
+
+				byte lightType = 255;
+				Scribe_Values.Look(ref lightType, "lightType", (byte) 255);
+
+				if (lightType == 255)
+				{
+					bool isDarklight = false;
+					Scribe_Values.Look(ref isDarklight, "isDarklight", false);
+
+					lightType = isDarklight ? (byte) FridgeLighting.LightType.Dark : (byte) FridgeLighting.LightType.Normal;
+				}
+
+				this.lightType = (FridgeLighting.LightType) (
+					  lightType <= (byte) FridgeLighting.LightType.Last
+					? lightType
+					: (byte) FridgeLighting.LightType.Normal
+				);
+			}
 		}
 	}
 }
